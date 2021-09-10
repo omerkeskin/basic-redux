dfadd5a1500ffa22430a3048bbf6bf87b7ea270a
Ömer Keskin <omer.keskin.deu@hotmail.com>
2021-09-11 00:16:55 +0300

    Redux es module import

--- src/index.js
+++ src/index.js
@@ -1,1 +1,17 @@
-console.log('Hello world');
+import {createStore}  from '../node_modules/redux/es/redux.mjs';
+
+ const reducer = (state = [], action) => {
+  console.log('reducer', state, action);
+  return state;
+};
+
+
+const store = createStore(reducer);
+
+// module 1
+store.subscribe(() => {
+  console.log('module 1 subscribe', store.getState());
+});
+
+// module 2
+store.dispatch({ type: 'ADD_USER'});
